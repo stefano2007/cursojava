424bbb452c01ab8e4b1206a37e3efa1152e63fff
stefano_2007 <stefano_2007@hotmail.com>
2022-02-20 18:23:27 -0300

    Projeto Criado

---
+++ src/test/java/com/stefanosilva/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefanosilva.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
